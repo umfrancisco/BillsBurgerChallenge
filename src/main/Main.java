b14f7d84cd90ae52b6ff99908eb2c7714d01dfde
umfrancisco <francis.guitler@gmail.com>
2025-10-15 15:57:05 -0300

    added implementations

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,12 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {
+		Item coke = new Item("drink", "coke", 1.50);
+		coke.printItem();
+		coke.setSize("large");
+		coke.printItem();
 		
+		Item fries = new Item("Topping", "fries", 1.50);
+		fries.printItem();
 	}
 }
