7305e2a3a951f922994ab5f29db5aa0105b2536e
umfrancisco <francis.guitler@gmail.com>
2025-10-16 08:23:03 -0300

    added DeluxeBurger.class

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,16 +2,19 @@ package main;
 
 public class Main {
 	public static void main(String[] args) {		
-		MealOrder meal = new MealOrder();
-		meal.addBurgerToppings("BACON", "CHEESE", "MAYO");
-		meal.setDrinkSize("LARGE");
-		meal.printItemizedList();
-		
-		System.out.println("*".repeat(30));
+//		MealOrder meal = new MealOrder();
+//		meal.addBurgerToppings("BACON", "CHEESE", "MAYO");
+//		meal.setDrinkSize("LARGE");
+//		meal.printItemizedList();
 		
 		MealOrder anotherMeal = new MealOrder("turkey", "guaraná", "chili");
 		anotherMeal.addBurgerToppings("LETTUCE", "CHEESE", "MAYO");
 		anotherMeal.setDrinkSize("SMALL");
 		anotherMeal.printItemizedList();
+		
+//		MealOrder deluxeMeal = new MealOrder("deluxe", "7-up", "chili");
+//		deluxeMeal.addBurgerToppings("AVOCADO", "BACON", "LETTUCE", "CHEESE", "MAYO");
+//		deluxeMeal.setDrinkSize("SMALL");
+//		deluxeMeal.printItemizedList();
 	}
 }
